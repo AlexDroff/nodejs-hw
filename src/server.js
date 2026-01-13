9551b62e0c5178072a23e2ffd4df9313e24895a8
Oleksandr Aleksandrov <alldroff@gmail.com>
2026-01-13 20:48:08 +0100

    upd server

--- src/server.js
+++ src/server.js
@@ -6,26 +6,37 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+// Глобальні middleware
 app.use(logger);
 app.use(express.json());
 app.use(cors());
 
+// Тестовий маршрут для Render
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+// Основні маршрути
 app.use(notesRoutes);
 
+// 404 — якщо маршрут не знайдено
 app.use(notFoundHandler);
 
+// Обробка помилок валідації celebrate
 app.use(errors());
 
+// Глобальна обробка помилок
 app.use(errorHandler);
 
+// Підключення до MongoDB
 await connectMongoDB();
 
+// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
