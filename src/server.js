4e53be1459470197da55111e33156e6fcf96f484
Oleksandr Aleksandrov <alldroff@gmail.com>
2026-01-25 14:46:11 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 // Основні маршрути
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 // 404 — якщо маршрут не знайдено
 app.use(notFoundHandler);
